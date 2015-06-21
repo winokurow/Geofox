256810f5e84e5e9d50e59143a14ce3ae13689b21
Ilja Winokurow <Ilja.Winokurow@clear-it.de>
2015-06-21 09:46:27 +0200

    New Game Activity

--- src/main/java/org/games/geofox/NewGameActivity.java
+++ src/main/java/org/games/geofox/NewGameActivity.java
@@ -1,25 +1,23 @@
 package org.games.geofox;
 
-import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
 
-public class MainActivity extends ActionBarActivity {
+public class NewGameActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_new_game);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_new_game, menu);
         return true;
     }
 
@@ -36,14 +34,5 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
-    }
-
-    /**
-     / Open Activity 'New Game' after click on the button 'Fox'.
-     /
-     */
-    public void gotoNewGame(View view) {
-        Intent intent = new Intent(this, NewGameActivity.class);
-        startActivity(intent);
     }
 }
